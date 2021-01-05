750867fe1761e5fe8eaff15850eaa56203bb6dbe
Park <pjane123@naver.com>
2021-01-05 23:49:09 +0900

    Update AdminSider

--- components/AdminSider.tsx
+++ components/AdminSider.tsx
@@ -1,37 +1,56 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { NextRouter, useRouter } from 'next/router';
 import { Layout, Menu } from 'antd';
-import { BarChartOutlined, UserOutlined, HomeOutlined } from '@ant-design/icons';
+
+import { BarChartOutlined, UserOutlined, HomeOutlined, SettingOutlined, DashboardOutlined, ApiOutlined } from '@ant-design/icons';
 
 import UserProfile from './UserProfile';
 import LoginForm from './LoginForm';
 
 import style from './adminSider.module.scss';
 
-interface Props {
-  setIsLoggedIn: boolean;
-}
-
 const { Sider } = Layout;
-const AdminSider: React.FC = () => {
+
+const siderStyle: React.CSSProperties = {
+  overflow: 'auto',
+  height: '100vh',
+  position: 'fixed',
+  left: 0,
+};
+
+const AdminSider: React.FC = ({}) => {
+  const router: NextRouter = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  console.log(router.pathname.split('/')[1]);
   return (
     <Layout className={style.customlayout}>
       {/* <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}> */}
-      <Sider className={style.adminSider} style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
+      <Sider className={style.adminSider} style={siderStyle}>
         <div className="logo" />
         {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
-        <Menu theme = "dark" className="sider-menu" style={{ backgroundColor: 'transparent' }} mode="inline" defaultSelectedKeys={['dashboard']}>
-          <Menu.Item className="custom" key="home" icon={<HomeOutlined />}>
-            <Link href="/">home</Link>
+        <Menu theme="dark" className="sider-menu" style={{ backgroundColor: 'transparent' }} mode="inline" selectedKeys={[router.pathname.split('/')[1]]}>
+          <Menu.Item className="custom" key="" icon={<HomeOutlined />}>
+            <Link href="/">Home</Link>
           </Menu.Item>
 
-          <Menu.Item  key="dashboard" icon={<BarChartOutlined />}>
-            <Link href="/dashboard">dashBoard</Link>
+          <Menu.Item key="dashboard" icon={<DashboardOutlined />}>
+            <Link href="/dashboard">DashBoard</Link>
           </Menu.Item>
 
-          <Menu.Item  key="userInfo" icon={<UserOutlined />}>
-            userInfo
+          <Menu.Item key="userInfo" icon={<UserOutlined />}>
+            UserInfo
+          </Menu.Item>
+
+          <Menu.Item key="api" icon={<ApiOutlined />}>
+            <Link href="/request">API request</Link>
+          </Menu.Item>
+          <Menu.Item key="chart" icon={<BarChartOutlined />}>
+            Chart
+          </Menu.Item>
+
+          <Menu.Item key="setting" icon={<SettingOutlined />}>
+            Setting
           </Menu.Item>
         </Menu>
       </Sider>
