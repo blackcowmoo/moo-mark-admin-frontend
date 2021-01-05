ee01852b57d3b88aed02421a3d8cc787fe53464c
Park <pjane123@naver.com>
2021-01-05 23:48:15 +0900

    add UserProfile style

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,14 +1,16 @@
 import React, { useCallback } from 'react';
 import { Card, Avatar, Button } from 'antd';
+import style from './userProfile.module.scss';
+
 const UserProfile: React.FC<any> = ({ setIsLoggedIn }) => {
   console.log(typeof setIsLoggedIn);
   const onLogOut = useCallback(() => {
     setIsLoggedIn(false);
   }, []);
   return (
-    <Card>
-      <Card.Meta avatar={<Avatar>admin</Avatar>} title="admin" />
-      <Button onClick={onLogOut}>로그아웃</Button>
+    <Card className={style.userProfile}>
+      <Card.Meta avatar={<Avatar>admin</Avatar>} title={<div className={style.profileName}>admin</div>} />
+      <Button className={style.profileButton} onClick={onLogOut}>로그아웃</Button>
     </Card>
   );
 };
