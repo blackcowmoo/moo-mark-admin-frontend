0e33371e985022dcfe0a2da7323644d896b33e81
Park <pjane123@naver.com>
2021-01-06 21:15:10 +0900

    update jest setting

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.jest.json',
-    },
-  },
+  setupFilesAfterEnv: ['./test-setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/'],
 };
